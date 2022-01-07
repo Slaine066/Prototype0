ed80935b365fe5d4b63e6ec76528e1ffec1a69cd
Gioi Giuseppe <giuseppe.gioi.1997@gmail.com>
2022-01-07 19:25:59 +0900

    Bug Fixes: 1. AI should turn to Player while attacking.

---
+++ Source/Prototype0/TurnToPlayerBTTask.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TurnToPlayerBTTask.h"
+
+#include "AIController.h"
+#include "HeroBase.h"
+#include "MobBase.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UTurnToPlayerBTTask::UTurnToPlayerBTTask()
+{
+	NodeName = TEXT("Turn To Player");
+}
+
+EBTNodeResult::Type UTurnToPlayerBTTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AMobBase* Mob = Cast<AMobBase>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!Mob) { return EBTNodeResult::Failed; }
+	
+	AHeroBase* Hero = Cast<AHeroBase>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("Player"));
+	if (!Hero) { return EBTNodeResult::Failed; }
+	
+	FVector LookVector = Hero->GetActorLocation() - Mob->GetActorLocation();
+	LookVector.Z = 0.0f;
+
+	const FRotator TargetRotation = FRotationMatrix::MakeFromX(LookVector).Rotator();
+	Mob->SetActorRotation(FMath::RInterpTo(Mob->GetActorRotation(), TargetRotation, GetWorld()->GetDeltaSeconds(), 2.0f));
+
+	return EBTNodeResult::Succeeded;
+}
